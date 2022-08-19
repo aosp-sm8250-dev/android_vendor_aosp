oneplus-sm8250-dev/android_vendor_proton

build: Add qcom_target.mk inclusion
Change-Id: Iad2f808f3e3eefbd2be2ca5facb907ff31babbb7

build/core/config.mk
@@ -1,0 +1,2 @@
+# Rules for QCOM targets
+include $(TOPDIR)vendor/proton/build/core/qcom_target.mk

aosp-sm8250-dev/android_vendor_aosp

build/core/config.mk
@@ -1,0 +1,2 @@
+# Rules for QCOM targets
+include $(TOPDIR)vendor/aosp/build/core/qcom_target.mk