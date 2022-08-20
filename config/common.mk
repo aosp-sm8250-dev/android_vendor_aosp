7332720c1e2d2abdea7a538ffa341e8b3e381dcc
Yuan Si <do4suki@gmail.com>
2022-08-20 09:06:13 +0800

    common: Build AOSP Messaging
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: I88682c3346b1ed4ae0e75024b40b1804961bd0c3

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,7 @@
 # APN configurations
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
+# Messaging
+PRODUCT_PACKAGES += \
+    messaging
