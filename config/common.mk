cb88c41d81c399372b75b8cf3d8a46331b747d98
Yuan Si <do4suki@gmail.com>
2022-08-20 09:08:02 +0800

    common: Ship GMS
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: I508baa4f12bcb7e4a3c87a996b38a55906e70fd9

--- config/common.mk
+++ config/common.mk
@@ -2,6 +2,17 @@
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 
+# GMS
+ifeq ($(WITH_GMS), true)
+$(call inherit-product, vendor/gms/core/core.mk)
+ifneq ($(WITH_GMS_CORE), true)
+$(call inherit-product, vendor/gms/extras/extras.mk)
+endif
+
+PRODUCT_PACKAGES += \
+    LatinIMEGooglePrebuilt
+endif
+
 # Messaging
 PRODUCT_PACKAGES += \
     messaging
